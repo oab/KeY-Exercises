70c71fe82b14232d441185bd9e9e9d616b8ab5bc
Ole Jørgen Abusdal <ojab@pc-guest3.se.informatik.tu-darmstadt.de>
2022-04-11 16:51:04 +0200

    Segment swap was fine

---
+++ SegmentSwap/src/SegmentSwap.java
@@ -0,0 +1,40 @@
+/*
+ * Here we make it slightly more difficult to write the loop invariant
+ * when compared to the SentinelSearch.java problem.
+ */
+
+class SegmentSwap {
+
+    /*@  public normal_behaviour
+     @  requires 1 < a.length && a.length < 10 && 0 <= startLow && endHigh < a.length &&
+     @           startLow <= endLow && startHigh <= endHigh && endLow < startHigh &&
+     @           endLow-startLow == endHigh-startHigh;
+     @  ensures (\forall int offset; 0 <= offset && offset <= endLow-startLow; 
+     @           a[startLow+offset] == \old(a[startHigh+offset]) && 
+     @           \old(a[startLow+offset]) == a[startHigh+offset]);
+     @  assignable a[*];
+    @*/
+    static void swap1(int a[], final int startLow, final int endLow, final int startHigh, final int endHigh) throws Exception {
+	int offset=0;
+	/*@ loop_invariant (0 <= offset && offset <= endLow-startLow+1) &&
+	  @ (\forall int o; 0 <= o && o < offset; 
+	  @                  a[startLow+o] == \old(a[startHigh+o]) && \old(a[startLow+o]) == a[startHigh+o]) &&
+	  @ (\forall int o; offset <= o && o <= endLow-startLow;
+	  @                  a[startLow+o] == \old(a[startLow+o]) && a[startHigh+o] == \old(a[startHigh+o]));
+          @                  
+	  @ decreases (endLow-startLow+1) - offset;
+	  @ assignable a[*];
+	  @*/
+	for(; offset <= endLow-startLow; offset++) {
+	    int i = startLow + offset;
+	    int j = startHigh + offset;
+	    
+	    int t = a[i];
+	    a[i] = a[j];
+	    a[j] = t;
+	}
+	
+    }
+ 
+    
+}
