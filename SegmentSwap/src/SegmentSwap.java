a04d250cb9faf9b8a3868b6fe83faafa3e04c214
Ole Jørgen Abusdal <ojab@pc-guest3.se.informatik.tu-darmstadt.de>
2022-04-11 17:28:15 +0200

    slight alteration and z3 is able to solve

--- SegmentSwap/src/SegmentSwap.java
+++ SegmentSwap/src/SegmentSwap.java
@@ -35,6 +35,36 @@ class SegmentSwap {
 	}
 	
     }
+
+    /*@  public normal_behaviour
+     @  requires 1 < a.length && a.length < 10 && 0 <= startLow && startHigh+size-1 < a.length &&
+     @           0 <= size && startLow+size-1 < startHigh;
+     @  ensures (\forall int offset; 0 <= offset && offset < size; 
+     @           a[startLow+offset] == \old(a[startHigh+offset]) && 
+     @           \old(a[startLow+offset]) == a[startHigh+offset]);
+     @  assignable a[*];
+    @*/
+    static void swap2(int a[], final int startLow, final int startHigh, final int size) throws Exception {
+	int offset=0;
+	/*@ loop_invariant (0 <= offset && offset <= size) &&
+	  @ (\forall int o; 0 <= o && o < offset; 
+	  @                  a[startLow+o] == \old(a[startHigh+o]) && \old(a[startLow+o]) == a[startHigh+o]) &&
+	  @ (\forall int o; offset <= o && o < size;
+	  @                  a[startLow+o] == \old(a[startLow+o]) && a[startHigh+o] == \old(a[startHigh+o]));
+          @                  
+	  @ decreases size - offset;
+	  @ assignable a[*];
+	  @*/
+	for(; offset < size; offset++) {
+	    int i = startLow + offset;
+	    int j = startHigh + offset;
+	    
+	    int t = a[i];
+	    a[i] = a[j];
+	    a[j] = t;
+	}
+	
+    }
  
     
 }
