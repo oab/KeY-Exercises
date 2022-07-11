5fa53c4be64de5244e815a4a59ca29ad39d9c9b9
Ole Jørgen Abusdal <ojab@pc-guest3.se.informatik.tu-darmstadt.de>
2022-07-11 10:51:55 +0200

    ok

--- UnionFind/unionfind/Elements.java
+++ UnionFind/unionfind/Elements.java
@@ -1,6 +1,6 @@
 package unionfind;
 
-public class Elements {
+public final class Elements {
 
     private int[] elements;
     private final int size;
@@ -8,39 +8,39 @@ public class Elements {
     protected Elements(int size) {
 	this.elements = new int[2*size];
 	this.size = size;
-	for(int i=0;i<elements.length;i+=2) {
-	    elements[i]=i/2;
+	for(int i=0,j=0;i<elements.length;i+=2,j++) {
+	    elements[i]=j;
 	    elements[i+1]=1;
 	    
 	}
     }
 
-    public int getSize() { return this.size; }
+    protected int getSize() { return this.size; }
     
-    public int getParent(final int i) {
+    protected int getParent(final int i) {
 	return elements[2*i];
     }
 
-    public int setParent(final int i, final int v) {
+    protected int setParent(final int i, final int v) {
  	return elements[2*i] = v;
     }
 
-    public int getSize(final int i) {
+    protected int getSize(final int i) {
       	return elements[2*i+1];
     }
 
-    public int setSize(final int i, final int v) {
+    protected int setSize(final int i, final int v) {
       	return elements[2*i+1] = v;
     }
     
-    public int findRoot(final int i) {
+    protected int findRoot(final int i) {
 	int e = i;
 	int old = i;
 	while((e = getParent(e)) != old) old = e;
 	return e;
     }
 
-    public void union(final int i,final int j) {
+    protected void union(final int i,final int j) {
 	
 	int x = findRoot(i);
 	int y = findRoot(j);
