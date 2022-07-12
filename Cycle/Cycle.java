c753bb37c7822044508137bbc1084b77cf63ca24
Ole Jørgen Abusdal <ojab@pc-guest3.se.informatik.tu-darmstadt.de>
2022-07-12 19:55:01 +0200

    ok

--- Cycle/Cycle.java
+++ Cycle/Cycle.java
@@ -10,14 +10,15 @@ public interface Cycle {
     
     //@ public accessible \inv: footprint;
  
-    
+    // if cycle is eL then it remains so, but returns Le
     /*@ public normal_behavior
       @ ensures cycle == \old(cycle);
       @ ensures \result.cycle == \seq_concat( \old(cycle[1..\old(size())]), \old(cycle[0..1]) );
       @ assignable footprint;
       @*/
     public Cycle /*@ pure @*/ next();
-    
+
+    // if the added is v and cycle is eL then cycle becomes evL, returns vLe
     /*@ public normal_behavior
       @ ensures cycle == \seq_concat( \seq_concat(\old(cycle[0..1]), \seq_singleton(value) ), \old(cycle[1..\old(size())])); 
       @ ensures \result.cycle == \seq_concat( \seq_concat(\seq_singleton(value), \old(cycle[1..\old(size())])), \old(cycle[0..1]) );
@@ -26,11 +27,12 @@ public interface Cycle {
       @*/
     public Cycle add(int value);
 
+    // if the cycle is size 1 do nothing, otherwise cycle is eL and becomes L
     /*@ public normal_behavior
       @ ensures cycle == ( (\old(size()) == 1) ? cycle : (cycle[1..\old(size())]) );
       @ assignable footprint;
       @*/
-    public Cycle remove();
+    public void remove();
 
     /*@ public normal_behavior
       @ ensures \result == cycle.length;
